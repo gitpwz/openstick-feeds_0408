9536b948fecd246f5612f78d90a0ba3caf0c8384
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-10 13:47:15 +0800

    Add files via upload

--- utils/gc/files/restart_gc.sh
+++ utils/gc/files/restart_gc.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-ROLE_PATH="/sys/class/udc/ci_hdrc.0/device/driver/ci_hdrc.0/role"
-MODE="$(uci get gc.config.mode 2>/dev/null)"
-
-if grep -q gadget "$ROLE_PATH" && [ "$MODE" = "gadget" ]; then
-	/etc/init.d/gc restart
+grep gadget /sys/class/udc/ci_hdrc.0/device/driver/ci_hdrc.0/role
+if [ $? -eq 0 ]
+then
+/etc/init.d/gc restart
 fi
